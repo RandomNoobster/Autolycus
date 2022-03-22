b3a81eed95c1885adcc21806c3abe167e47f9ef0
Randy <kriosen03@outlook.com>
2022-03-22 20:07:24 +0100

    Adding who()

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ import os
 from discord.commands import Option
 from discord.bot import ApplicationCommandMixin
 import re
+from threading import Thread
 import json
+import math
 import pathlib
+import utils
 import time
 import discord
 from discord.ext import commands
@@ -60,9 +63,99 @@ async def ping(ctx: discord.ApplicationContext):
     await ctx.respond(f'Pong! {round(bot.latency * 1000)}ms')
 
 @bot.slash_command(
+    name="who",
+    description="Get more information about someone's nation",
+)
+async def who(
+    ctx: discord.ApplicationContext,
+    person: Option(str, "") = None,
+):
+    await ctx.defer()
+    if person == None:
+        person = ctx.author.id
+    nation = utils.find_nation_plus(bot, person)
+    if nation == None:
+        await ctx.respond(content="I did not find that nation!")
+        return
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"https://api.politicsandwar.com/graphql?api_key={api_key}", json={"query": f"{{nations(first:1 id:{nation['id']}){{data{{id nation_name leader_name num_cities cia spy_satellite warpolicy population dompolicy flag vmode color beigeturns last_active soldiers tanks aircraft ships nukes missiles mlp nrf vds irond wars{{attid turnsleft}} cities{{barracks factory airforcebase drydock}} score alliance_position alliance_seniority alliance{{name id score color nations{{id}}}}}}}}}}"}) as temp:
+            nation = (await temp.json())['data']['nations']['data'][0]
+
+    embed = discord.Embed(title=nation['nation_name'], url=f"https://politicsandwar.com/nation/id={nation['id']}", color=0xff5100)
+    user = utils.find_user(bot, nation['id'])
+    if user == None:
+        discord_info = "> Verified: <:redcross:862669500977905694>"
+        if nation['discord']:
+            discord_info += f"> Discord Username: {nation['discord']}"
+    else:
+        username = await bot.fetch_user(user['user'])
+        discord_info = f"> Verified: ✅\n> Discord Username: {username}"
+    embed.add_field(name="Discord Info", value=discord_info, inline=False)
+
+    nation_info = f"> Nation Name: {nation['nation_name']}\n> Leader Name: {nation['leader_name']}\n> Cities: [{nation['num_cities']}](https://politicsandwar.com/city/manager/n={nation['nation_name']})\n> War Policy: {nation['warpolicy']}\n> Domestic Policy: {nation['dompolicy']}"
+    embed.add_field(name="Nation Info", value=nation_info)
+
+    nation_info_2 = f"> Score: {nation['score']}\n> Def. Range: {round(nation['score']/1.75)} - {round(nation['score']/0.75)}\n> Off. Range: {round(nation['score']*0.75)} - {round(nation['score']*1.75)}\n> Color: {nation['color'].capitalize()}\n> Turns of VM: {nation['vmode']}"
+    embed.add_field(name="\u200b", value=nation_info_2)
+
+    if nation['alliance']:
+        alliance_info = f"> Alliance: [{nation['alliance']['name']}](https://politicsandwar.com/alliance/id={nation['alliance']['id']})\n> Position: {nation['alliance_position'].capitalize()}\n> Score: {nation['alliance']['score']:,}\n> Color: {nation['alliance']['color'].capitalize()}\n> Members: {len(nation['alliance']['nations'])}"
+    else:
+        alliance_info = f"> Alliance: None"
+    embed.add_field(name="Alliance Info", value=alliance_info, inline=False)
+
+    spy_count = await utils.spy_calc(nation)
+    if nation['spy_satellite']:
+        daily_rebuy = 3
+    else:
+        daily_rebuy = 2
+    if nation['cia']:
+        max_spies = 60
+    else:
+        max_spies = 50
+    spies = f"{spy_count} / {max_spies} / {math.ceil((max_spies-spy_count)/daily_rebuy)}"
+
+    milt = utils.militarization_checker(nation)
+    military_info = f"> Format: Current / Cap / Days\n> Soldiers: {nation['soldiers']:,} / {milt['max_soldiers']:,} / {milt['soldiers_days']:,}\n> Tanks: {nation['tanks']:,} / {milt['max_tanks']:,} / {milt['tanks_days']:,}\n> Aircraft: {nation['aircraft']:,} / {milt['max_aircraft']:,} / {milt['aircraft_days']:,}\n> Ships: {nation['ships']:,} / {milt['max_ships']:,} / {milt['ships_days']:,}\n> Spies: {spies}\n> MMR: {milt['barracks_mmr']} / {milt['factory_mmr']} / {milt['hangar_mmr']} / {milt['drydock_mmr']}"
+    embed.add_field(name="Military Info", value=military_info)
+
+    missiles = str(nation['missiles'])
+    if not nation['mlp']:
+        missiles += " (No Project)"
+    nukes = str(nation['nukes'])
+    if not nation['nrf']:
+        nukes += " (No Project)"
+
+    o_wars = 0
+    d_wars = 0
+    for war in nation['wars']:
+        if war['turnsleft'] > 0:
+            if war['attid'] == nation['id']:
+                o_wars += 1
+            else:
+                d_wars += 1
+
+    if nation['irond']:
+        dome = "Yes"
+    else:
+        dome = "No"
+    if nation['vds']:
+        vital = "Yes"
+    else:
+        vital = "No"
+
+    military_info_2 = f"> Offensive Wars: {o_wars} / 5\n> Defensive Wars: {d_wars} / 3\n> Missiles: {missiles}\n> Nukes: {nukes}\n> Iron Dome: {dome}\n> Vital Defense: {vital}\n> Turns of Beige: {nation['beigeturns']}"
+    embed.add_field(name="\u200b", value=military_info_2)
+
+    embed.set_thumbnail(url=nation['flag'])
+
+    await ctx.respond(embed=embed)
+
+@bot.slash_command(
     name="verify",
     description='Link your nation with your discord account',
-    )
+)
 async def verify(
     ctx: discord.ApplicationContext,
     nation_id: Option(str, "Your nation id or nation link"),
@@ -130,7 +223,6 @@ async def nation_scanner():
     await bot.wait_until_ready()
     debug_channel = bot.get_channel(channel_id)
     while True:
-        now = time.time()
         try:
             async with aiohttp.ClientSession() as session:
                 more_pages = True
@@ -138,9 +230,8 @@ async def nation_scanner():
                 first = 50
                 new_nations = {"last_fetched": None, "nations": []}
                 while more_pages:
-                    #print(f"call: {n}, nations: {len(new_nations['nations'])}")
                     try:
-                        await asyncio.sleep(1)
+                        await asyncio.sleep(2)
                         async with session.post(f"https://api.politicsandwar.com/graphql?api_key={api_key}", json={'query': f"{{nations(page:{n} first:{first} vmode:false orderBy:{{column:DATE order:ASC}}){{paginatorInfo{{hasMorePages}} data{{id discord leader_name nation_name flag last_active continent dompolicy population alliance_id beigeturns score color soldiers tanks aircraft ships missiles nukes bounties{{amount war_type}} treasures{{name}} alliance{{name}} wars{{date winner defid turnsleft attacks{{loot_info victor moneystolen}}}} alliance_position num_cities ironw bauxitew armss egr massirr itc recycling_initiative telecom_satellite green_tech clinical_research_center specialized_police_training uap cities{{date powered infrastructure land oilpower windpower coalpower nuclearpower coalmine oilwell uramine barracks farm policestation hospital recyclingcenter subway supermarket bank mall stadium leadmine ironmine bauxitemine gasrefinery aluminumrefinery steelmill munitionsfactory factory airforcebase drydock}}}}}}}}"}) as temp:
                             resp = await temp.json()
                             new_nations['nations'] += resp['data']['nations']['data']
@@ -154,10 +245,10 @@ async def nation_scanner():
                     json.dump(new_nations, json_file)
         except Exception as error:
             await debug_channel.send(f'**Exception raised!**\nWhere: Scanning nations\n\nError:```{error}```')
-        #print(f"round done! that took {now - time.time()} seconds")
 
 keep_alive.run()
 
-bot.bg_task = bot.loop.create_task(alert_scanner())
-bot.bg_task = bot.loop.create_task(nation_scanner())
 bot.run(os.getenv("bot_token"))
+
+Thread(target=asyncio.run, args=(nation_scanner(),)).start()
+Thread(target=asyncio.run, args=(alert_scanner(),)).start()
