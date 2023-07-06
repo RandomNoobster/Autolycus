da21db66de006d044c703b70cd674075ba009fa6
Randy <kriosen03@outlook.com>
2023-07-07 00:22:33 +0200

    removing alerts once sent

--- cogs/background.py
+++ cogs/background.py
@@ -72,6 +72,7 @@ class General(commands.Cog):
 
                         if pull:
                             await async_mongo.global_users.find_one_and_update({"user": user['user']}, {"$pull": {"beige_alerts": alert}})
+                            user['beige_alerts'].remove(alert)
                             logger.info("Reminders (1.7)")
                         break
 
